931028049d8ff1d2dc9acd1668d5c1b47f34f748
NguyenCuongLuc <luc.nguyen@orientsoftware.net>
2019-07-14 23:43:01 +0700

    init project

---
+++ DailyQuote/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {
+  View,
+  Text,
+} from 'react-native';
+
+
+const App = () => {
+  return (
+   <View>
+     <Text>Hello Mikki!</Text>
+   </View>
+  );
+};
+
+export default App;
