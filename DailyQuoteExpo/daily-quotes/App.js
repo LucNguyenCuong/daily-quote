3c3230de149e751a3c0b2f5bbe74889594226a8c
NguyenCuongLuc <luc.nguyen@orientsoftware.net>
2019-08-06 23:03:24 +0700

    test app

---
+++ DailyQuoteExpo/daily-quotes/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Home from './src/components/home.js'
+
+export default function App() {
+  return (
+    <Home />
+  );
+}
