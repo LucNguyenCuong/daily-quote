5b558fb91b6fbbdcd06e4095f95cfe8532a8dffa
NguyenCuongLuc <luc.nguyen@orientsoftware.net>
2019-08-07 00:08:56 +0700

    test

--- DailyQuoteExpo/daily-quotes/src/components/home.js
+++ DailyQuoteExpo/daily-quotes/src/components/home.js
@@ -1,35 +1,67 @@
 import React, { Component } from 'react';
-import { ScrollView } from 'react-native'
+import { ScrollView, View, Text } from 'react-native'
 import Quote from './quote';
 
 class Home extends Component {
-  state = { quotes: [] };
+
+  state = { quote: { "id": "", "text": "" }, time: '' };
 
   componentWillMount() {
-    this.setState({ quotes: quotes });
+    // this.setState({ quotes: quotes });
+
+    quotes.map(quote => {
+      setInterval(() =>
+        this.setState({ quote: quote })
+        , 1000);
+    })
+
+    // this.interval = setInterval(() => this.setState({ time: Date.now() }), 1000);
   }
 
-  renderQuotes() {
-    return this.state.quotes.map(quote =>
-      <Quote key={quote.id} props={quote}/>
-    );
+  componentWillUnmount() {
+    // clearInterval(this.interval);
   }
+
+  // renderQuotes() {
+
+  //   return this.state.quotes.map(quote =>
+  //     <Quote key={quote.id} props={quote} />
+  //   );
+  // }
 
   render() {
+    const { viewStyle, textStyle } = style;
+    //  return this.state.quotes.map(quote => {
+    //     return (<View key={quote.id}>
+    //       <Quote  props={quote} />
+    //     </View>)
+    //   });
     return (
-      <ScrollView>
-        {this.renderQuotes()}
-      </ScrollView>
-    )
+      <View id={this.state.quote.id} style={viewStyle}>
+        <Text style={textStyle}> {this.state.quote} </Text>
+      </View>
+    );
   }
 }
 
 const quotes = [
-  { "id": "1", "text": "meowww 123123" },
+  { "id": "1", "text": "meowww 1231233" },
   { "id": "2", "text": "meowww1" },
   { "id": "3", "text": "meowww2" },
   { "id": "4", "text": "meowww3" },
   { "id": "5", "text": "meowww4" }
 ]
+
+const style = {
+  viewStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 60,
+    paddingTop: 15,
+  },
+  textStyle: {
+    fontSize: 20
+  }
+}
 
 export default Home;
